3749ddd8bf9dbdd4c74d366a22ebb36987f39acb
CianDickerHughes <G00415413@atu.ie>
2024-12-24 21:03:28 +0000

    add navigation and pages

--- index.js
+++ index.js
@@ -14,3 +14,18 @@ app.get('/', (req, res, next) => {
     console.log("GET receieved on /");
     res.render("Home")
 });
+
+app.get('/Students', (req, res, next) => {
+    console.log("GET receieved on /Students");
+    res.render("Students")
+});
+
+app.get('/Grades', (req, res, next) => {
+    console.log("GET receieved on /Grades");
+    res.render("Grades")
+});
+
+app.get('/Lecturers', (req, res, next) => {
+    console.log("GET receieved on /Lecturers");
+    res.render("Lecturers")
+});
